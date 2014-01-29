a5050621a2cc041f988178a1d64d38fa6d00c543
Mr.Rabbit <attn.mr.rabbit@gmail.com>
2014-01-28 23:04:24 -0500

    +javadoc

--- src/main/java/org/tinyfix/latency/protocols/CorrelationIdExtractor.java
+++ src/main/java/org/tinyfix/latency/protocols/CorrelationIdExtractor.java
@@ -5,6 +5,12 @@ import org.jnetpcap.packet.JPacket;
 /** Extracts correlation ID from JPacket */
 public interface CorrelationIdExtractor<T> {
 
+    /**
+     * @param packet packet to be parsed
+     * @param start payload start offset
+     * @param len payload length
+     * @param cookie optional cookie
+     */
     void parse(JPacket packet, int start, int len, T cookie);
 
 }
